9aa87f82b19586839fc9a43989fb551e09bd77c9
Atharva Pingale <atharva0300@gmail.com>
2023-04-15 23:26:49 +0530

    refoctoring and updating the file

--- redux-and-redux-toolkit/src/App.js
+++ redux-and-redux-toolkit/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-      <h2>This is a Reactjs App</h2>
+      <h2>This is an App</h2>
     </div>
   );
 }
